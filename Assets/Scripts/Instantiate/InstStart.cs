745e57433ee0e81bd4b72dbbd7e4d1cf54c69a0a
Vincent-LeVen <32410365+Vincent-LeVen@users.noreply.github.com>
2018-11-24 17:24:26 +0100

    2.0 added energy_new bridges_start code

---
+++ Assets/Scripts/Instantiate/InstStart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstStart : MonoBehaviour {
+
+    public GameObject blockStart;
+    
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetMouseButton(0))
+        {
+            Instantiate (blockStart, transform.position, transform.rotation);
+        }
+		
+	}
+}
